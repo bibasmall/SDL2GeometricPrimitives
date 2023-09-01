fe91ae1638fe6e9d06ec31ce6b6918ea7e183d57
bibasmall <hgausn@gmail.com>
2023-09-01 15:48:36 +0300

    raw pointers replaced with smart ones, code improved

--- SDL2GeometricPrimitives/core.h
+++ SDL2GeometricPrimitives/core.h
@@ -5,14 +5,15 @@
 #include <array>
 #include <tuple>
 #include <SDL.h>
+#include <assert.h>
 #include <SDL_ttf.h>
+#include <functional>
 
 
 class IPrimitive
 {
 public:
-	IPrimitive() {}
-	virtual ~IPrimitive() {};
+	virtual ~IPrimitive() {}
 
 public:
 	virtual void Draw(SDL_Renderer*) const = 0;
@@ -21,8 +22,6 @@ public:
 class Triangle : public IPrimitive
 {
 public:
-	Triangle() = delete;
-	
 	Triangle(SDL_FPoint x, SDL_FPoint y, SDL_FPoint z) : IPrimitive(), verts{ SDL_Vertex{ x, SDL_Color{ 255, 0, 0, 255 }, SDL_FPoint{ 0 } },
 																			  SDL_Vertex{ y, SDL_Color{ 0, 0, 255, 255 }, SDL_FPoint{ 0 } },
 																			  SDL_Vertex{ z, SDL_Color{ 0, 255, 0, 255 }, SDL_FPoint{ 0 } } }
@@ -36,21 +35,20 @@ public:
 	
 	}
 	Triangle(SDL_FPoint x, SDL_FPoint y, SDL_FPoint z, SDL_Color xc, SDL_Color yc, SDL_Color zc)
-		: IPrimitive(), verts{ SDL_Vertex{ x, SDL_Color{ xc }, SDL_FPoint{ 0 } },
-								 SDL_Vertex{ y, SDL_Color{ yc }, SDL_FPoint{ 0 } },
-								 SDL_Vertex{ z, SDL_Color{ zc }, SDL_FPoint{ 0 } } } 
+		: IPrimitive(), verts{	SDL_Vertex{ x, SDL_Color{ xc }, SDL_FPoint{ 0 } }, 
+								SDL_Vertex{ y, SDL_Color{ yc }, SDL_FPoint{ 0 } },
+								SDL_Vertex{ z, SDL_Color{ zc }, SDL_FPoint{ 0 } } } 
 	{
 
 	}
 
-	~Triangle() {}
+	~Triangle() = default;
 
 
 public:
 	void Draw(SDL_Renderer* renderer) const override
 	{
-		if (!renderer)
-			return;
+		assert(renderer);
 		SDL_RenderGeometry(renderer, nullptr, verts.data(), verts.size(), nullptr, 0);
 	}
 
@@ -62,20 +60,16 @@ class Rectangle : public IPrimitive
 {
 public:
 	Rectangle() = delete;
-	Rectangle(int w, int h, int x, int y, SDL_Color color = { 255, 255, 255, 255 }) : rect{}, color{color}
+	Rectangle(int w, int h, int x, int y, SDL_Color color = { 255, 255, 255, 255 }) : rect{.x = x, .y = y, .w = w, .h = h}, color{color} 
 	{
-		rect.x = x;
-		rect.y = y;
-		rect.w = w;
-		rect.h = h;
+
 	}
-	~Rectangle() {}
+	~Rectangle() = default;
 
 public:
 	void Draw(SDL_Renderer* renderer) const override
 	{
-		if (!renderer)
-			return;
+		assert(renderer);
 		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 		SDL_RenderFillRect(renderer, &rect);
 	}
@@ -96,14 +90,12 @@ class Circle : public IPrimitive
 public:
 	Circle() = delete;
 	Circle(int x, int y, int radius, SDL_Color color = { 255, 255, 255, 255 }) : center{x, y}, radius{radius}, color{color} {}
-	~Circle() {}
+	~Circle() = default;
 
 public:
 	void Draw(SDL_Renderer* renderer) const override
 	{
-		if (!renderer)
-			return;
-
+		assert(renderer);
 		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 		for (int w = 0; w < radius * 2; w++)
 		{
@@ -129,21 +121,29 @@ class SDLBox
 public:
 	SDLBox()
 	{
-		SDL_Init(SDL_INIT_EVERYTHING);
-		window = SDL_CreateWindow("SDL2GeometricPrimitives", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, SDL_WINDOW_SHOWN);
-		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		auto errorInitSDL = SDL_Init(SDL_INIT_EVERYTHING);
+		assert(!errorInitSDL);
+		auto errorInitTTF = TTF_Init();
+		assert(!errorInitTTF);
+		window = std::unique_ptr<SDL_Window, std::function<void(SDL_Window*)>>(
+			SDL_CreateWindow("SDL2GeometricPrimitives", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, SDL_WINDOW_SHOWN),
+			[](SDL_Window* wnd) { SDL_DestroyWindow(wnd); });
+		assert(window);
+		renderer = std::unique_ptr<SDL_Renderer, std::function<void(SDL_Renderer*)>>(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC),
+												  [](SDL_Renderer* renderer) { SDL_DestroyRenderer(renderer); });
+		assert(renderer);
 
-		TTF_Init();
-		TTF_Font* sans = TTF_OpenFont("./data/FreeSans.ttf", 70);
-		surfaceMessage = TTF_RenderText_Solid(sans, "press ESC to quit", { 255, 255, 255 });
-		message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+		auto font = TTF_OpenFont("./data/FreeSans.ttf", 70);
+		assert(font);
+		surfaceMessage = std::unique_ptr<SDL_Surface, std::function<void(SDL_Surface*)>>(TTF_RenderText_Solid(font, "press ESC to quit", { 255, 255, 255 }),
+													   [](SDL_Surface* surfaceMessage) { SDL_FreeSurface(surfaceMessage); });
+		assert(surfaceMessage);
+		message = std::unique_ptr<SDL_Texture, std::function<void(SDL_Texture*)>>(SDL_CreateTextureFromSurface(renderer.get(), surfaceMessage.get()),
+												[](SDL_Texture* message){ SDL_DestroyTexture(message); });
+		assert(message);
 	}
 	~SDLBox()
 	{
-		SDL_FreeSurface(surfaceMessage);
-		SDL_DestroyTexture(message);
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
 		TTF_Quit();
 		SDL_Quit();
 	}
@@ -157,50 +157,46 @@ public:
 public:
 	void Run() const
 	{
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
+		SDL_RenderClear(renderer.get());
 		for (auto& p : primitives)
-			p->Draw(renderer);
+			p->Draw(renderer.get());
+		
+		SDL_Rect Message_rect;
+		Message_rect.x = 10;
+		Message_rect.y = 10;
+		Message_rect.w = 300;
+		Message_rect.h = 70;
+		SDL_RenderCopy(renderer.get(), message.get(), NULL, &Message_rect);		
 
-		{
-			SDL_Rect Message_rect;
-			Message_rect.x = 10;
-			Message_rect.y = 10;
-			Message_rect.w = 300;
-			Message_rect.h = 70;
-			SDL_RenderCopy(renderer, message, NULL, &Message_rect);
-		}
-
-		bool running = true;
-		while (running)
-		{
-			SDL_Event ev;
-			while (SDL_PollEvent(&ev))
-			{
-				if ((SDL_QUIT == ev.type) ||
-					(SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode))
-				{
-					running = false;
-					break;
-				}
-			}
-
-			SDL_RenderPresent(renderer);
-		}
+		SDL_RenderPresent(renderer.get());
+		WaitForESC();
+		
 	}
 	void PushPrimitive(std::unique_ptr<IPrimitive> primitive)
 	{
 		primitives.push_back(std::move(primitive));
 	}
+	
+private:
+	void WaitForESC() const
+	{
+		while (true)
+		{
+			SDL_Event ev;
+			while (SDL_PollEvent(&ev))
+			{
+				if ((SDL_QUIT == ev.type) || (SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode))
+					return;
+			}
+		}
+	}
 
 private:
-
-
-private:
-	SDL_Window*								 window = nullptr;
-	SDL_Renderer*							 renderer = nullptr;
-	SDL_Surface*							 surfaceMessage = nullptr;
-	SDL_Texture*							 message = nullptr;
-	std::vector<std::unique_ptr<IPrimitive>> primitives = {};
+	std::unique_ptr<SDL_Texture, std::function<void(SDL_Texture*)>>		message;
+	std::unique_ptr<SDL_Surface, std::function<void(SDL_Surface*)>>		surfaceMessage;
+	std::vector<std::unique_ptr<IPrimitive>>							primitives;
+	std::unique_ptr<SDL_Window, std::function<void(SDL_Window*)>>		window;
+	std::unique_ptr<SDL_Renderer, std::function<void(SDL_Renderer*)>>	renderer;
 };
 
